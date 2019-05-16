84df1d2902e87c42e21b56c613236a329f3d4ef0
Dimitri Otero <doterodesign@gmail.com>
2019-05-16 08:06:58 -0400

    node babel-loader

---
+++ build/precache-manifest.520029c4da99d46b9218ed4e25624e52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0616e876e1c45110e298632a492b91ea",
+    "url": "/d-otero/cunyfirst-react/index.html"
+  },
+  {
+    "revision": "6d611c6cbc7d2ee383cd",
+    "url": "/d-otero/cunyfirst-react/static/css/main.8be2ab9a.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/d-otero/cunyfirst-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6d611c6cbc7d2ee383cd",
+    "url": "/d-otero/cunyfirst-react/static/js/main.22b0d39e.chunk.js"
+  },
+  {
+    "revision": "5e13e875e616cdcf31b5",
+    "url": "/d-otero/cunyfirst-react/static/js/runtime~main.12a6afa8.js"
+  },
+  {
+    "revision": "df9b04c05a5e0700b36eeed1aa2270a3",
+    "url": "/d-otero/cunyfirst-react/static/media/arrow-with-circle-down.df9b04c0.svg"
+  },
+  {
+    "revision": "a7662c1c0fa2e94b171ca0463069fa2c",
+    "url": "/d-otero/cunyfirst-react/static/media/arrow-with-circle.a7662c1c.svg"
+  }
+]);
